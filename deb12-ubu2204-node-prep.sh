9f68f59d666263acde0934efd198a25c1c56b62d
ecamposh <82472401+ecamposh@users.noreply.github.com>
2025-06-27 16:13:37 -0600

    deb12-ubu2204-node-prep.sh

--- deb12-ubu2204-node-prep.sh
+++ deb12-ubu2204-node-prep.sh
@@ -6,7 +6,7 @@
 # Reference: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
 # Reference: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/kubelet-integration/
 
-# Always upgrade first your system before running this bash script sudo apt update && sudo apt upgrade -y
+# Always upgrade first your system before running this bash script 'sudo apt update && sudo apt upgrade -y'
 
 # Define version for packages needed
 KUBERNETES_VERSION=v1.33
